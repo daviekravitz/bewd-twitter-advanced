a520c945f3c971a55f42595fde660de100c22609
Davie Kravitz <davie@Davies-Mac-mini.local>
2024-01-16 14:34:22 -0600

    configures AWS blob storage and mailgun for succesful tweets

--- Gemfile
+++ Gemfile
@@ -71,5 +71,5 @@ end
 
 group :production do
   # Use pg as the database for Active Record
-  gem 'pg', '~> 1.4.3'
+  gem 'sqlite3', '~> 1.4'
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'davie_kravitz@kleinschmidtinc.com'
   layout 'mailer'
 end
